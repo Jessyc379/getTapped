5c05e16682eb1ee6bd1346f4472641f465038fe2
Valerie Kuan <valeriekuan01@gmail.com>
2024-10-03 13:13:03 -0700

    added more data in database and updated brewer id

--- spring-boot-api-server/database/create_database.sql
+++ spring-boot-api-server/database/create_database.sql
@@ -50,7 +50,7 @@ CREATE TABLE Brewery (
     latitude DOUBLE,
     phone VARCHAR(15),
     website_url VARCHAR(255),
-    owner_id INT,
+    brewer_id INT,
     FOREIGN KEY (brewer_id) REFERENCES Brewer(brewer_id)
 );
 
@@ -91,7 +91,7 @@ VALUES  ('user','$2a$10$NkufUPF3V8dEPSZeo1fzHe9ScBu.LOay9S3N32M84yuUM2OJYEJ/.','
 ('gandalf','$2a$10$lfQi9jSfhZZhfS6/Kyzv3u3418IgnWXWDQDk7IbcwlCFPgxg9Iud2','ROLE_ADMIN'),
 ('frodo','$2a$10$lfQi9jSfhZZhfS6/Kyzv3u3418IgnWXWDQDk7IbcwlCFPgxg9Iud2','ROLE_BREWER'),
 ('samwise','$2a$10$lfQi9jSfhZZhfS6/Kyzv3u3418IgnWXWDQDk7IbcwlCFPgxg9Iud2','ROLE_BREWER'),
-('gollum','$2a$10$lfQi9jSfhZZhfS6/Kyzv3u3418IgnWXWDQDk7IbcwlCFPgxg9Iud2','ROLE_USER')
+('gollum','$2a$10$lfQi9jSfhZZhfS6/Kyzv3u3418IgnWXWDQDk7IbcwlCFPgxg9Iud2','ROLE_USER'),
 ('thorin', '$2a$10$JHQg13RccfC2He9Bz6a8m.5RYF5B0IdHmnEbt/gSuV8wWfxTUIFqG', 'ROLE_USER'),
 ('legolas', '$2a$10$E5cbsTKwDpU1RuHKuyOguu68Aqz8gn8c74N5sk6ZXs6QDLkq0IlhG', 'ROLE_USER'),
 ('arwen', '$2a$10$hp87ED0gZy2SgSKV5u3Y4OHRL1h2KpI5MRV8PeyBeHo8Rp8AHgRMK', 'ROLE_USER'),
@@ -101,3 +101,41 @@ VALUES  ('user','$2a$10$NkufUPF3V8dEPSZeo1fzHe9ScBu.LOay9S3N32M84yuUM2OJYEJ/.','
 ('barleycorn', '$2a$10$Nf7hlFEV9G4L5BWCSukD9OUtJxNe9VB1JpN8y0b3Bgs39h9RDd2m.', 'ROLE_BREWER'),
 ('hobbiton', '$2a$10$fuTwZsTfHSr0RrzKyHQddO8V4dIsQjx8zH2MSpTkSOE.EGttqjGsi', 'ROLE_USER'),
 ('rivendell', '$2a$10$GtQWyKEQp7DZ/xU0pAfKx.4U9Aa9Af68hRhH/6BSF3PrQQhx9Nqhm', 'ROLE_USER');
+
+
+INSERT INTO Brewer (brewer_id, breweries_owned, user_id) VALUES
+(1, 2, 4),   -- Frodo (Brewer)
+(2, 1, 5),   -- Samwise (Brewer)
+(3, 1, 11),  -- Dumbledore (Brewer)
+(4, 3, 12),  -- Barleycorn (Brewer)
+(5, 1, 3);   -- Gandalf (Admin with Brewer role)
+
+INSERT INTO Customer (customer_id, favorite_breweries, total_reviews, user_id) VALUES
+(1, '["b0e7df8b-c2b3-4824-8b3c-c12e3a1a1fda"]', 5, 1),  -- User (Customer)
+(2, '["3f76421d-4d5b-4ae3-b3f0-bd6f3cf42ab8"]', 2, 2),  -- Admin (Customer)
+(3, '["b0e7df8b-c2b3-4824-8b3c-c12e3a1a1fda"]', 8, 6),     -- Gollum (Customer)
+(4, '["b0e7df8b-c2b3-4824-8b3c-c12e3a1a1fda"]', 3, 7),  -- Thorin (Customer)
+(5, '["3f76421d-4d5b-4ae3-b3f0-bd6f3cf42ab8"]', 4, 8);   -- Legolas (Customer)
+
+INSERT INTO Brewery (brewery_id, name, brewery_type, address, city, state_province, postal_code, country, longitude, latitude, phone, website_url, owner_id)
+VALUES
+('b0e7df8b-c2b3-4824-8b3c-c12e3a1a1fda', 'Twilight Brewing Company', 'brewpub', '2002 Shadow Lane', 'Seattle', 'Washington', '98101', 'United States', -122.330052, 47.606209, '206-555-0182', 'http://www.twilightbrewing.com', 2),
+('3f76421d-4d5b-4ae3-b3f0-bd6f3cf42ab8', 'Sunrise Craft Brewery', 'micro', '45 Sunrise Ave', 'Phoenix', 'Arizona', '85001', 'United States', -112.074036, 33.448376, '602-555-1234', 'http://www.sunrisecraftbrewery.com', 3);
+
+
+INSERT INTO CustomerFavorites (favorite_id, customer_id, brewery_id)
+VALUES
+(1, 1, 'b0e7df8b-c2b3-4824-8b3c-c12e3a1a1fda'),
+(2, 2, 'b0e7df8b-c2b3-4824-8b3c-c12e3a1a1fda'),
+(3, 3, '3f76421d-4d5b-4ae3-b3f0-bd6f3cf42ab8'),
+(4, 4, '3f76421d-4d5b-4ae3-b3f0-bd6f3cf42ab8');
+
+
+INSERT INTO CustomerReviews (review_id, customer_id, brewery_id, rating, review, date)
+VALUES
+(1, 1, 'b0e7df8b-c2b3-4824-8b3c-c12e3a1a1fda', 4, 'Great atmosphere and amazing stouts!', '2024-09-30'),
+(2, 2, 'b0e7df8b-c2b3-4824-8b3c-c12e3a1a1fda', 5, 'Best IPAs in town! Highly recommend.', '2024-09-28'),
+(3, 3, '3f76421d-4d5b-4ae3-b3f0-bd6f3cf42ab8', 5, 'Fantastic selection of seasonal beers!', '2024-09-29'),
+(4, 4, '3f76421d-4d5b-4ae3-b3f0-bd6f3cf42ab8', 4, 'Loved the ambiance and live music!', '2024-09-27');
+
+
