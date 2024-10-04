387f2107692ab526ae070dbe4983e20434562c58
Jessy Cesena <jessikah.cesena@gmail.com>
2024-10-04 09:03:28 -0700

    Added @Repository @Override to brewer

--- spring-boot-api-server/src/main/java/com/niantic/data/MySqlBrewerDao.java
+++ spring-boot-api-server/src/main/java/com/niantic/data/MySqlBrewerDao.java
@@ -1,10 +1,12 @@
 package com.niantic.data;
 
 import com.niantic.models.Brewer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.jdbc.support.rowset.SqlRowSet;
+import org.springframework.stereotype.Repository;
 
 import javax.sql.DataSource;
 import java.sql.PreparedStatement;
@@ -12,17 +14,18 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
-
+@Repository
 public class MySqlBrewerDao implements BrewerDao {
     private final JdbcTemplate jdbcTemplate;
 
+    @Autowired
     public MySqlBrewerDao(DataSource dataSource) {
         jdbcTemplate = new JdbcTemplate(dataSource);
     }
 
 
     @Override
-    public List<Brewer> getAll(int brewerId) {
+    public List<Brewer> getAll() {
         List<Brewer> brewers = new ArrayList<>();
 
         String sql = """
