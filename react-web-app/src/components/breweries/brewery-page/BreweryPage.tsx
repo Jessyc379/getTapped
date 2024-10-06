b04e0346d3d418d959e71e879be6456785082b2a
Karen Delarosa <kaydelarose@gmail.com>
2024-10-06 13:21:28 -0700

    created routing for BreweryPage

---
+++ react-web-app/src/components/breweries/brewery-page/BreweryPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BreweryPage = () => {
+    return (
+        <div>
+            <h1>Brewery Page</h1>
+
+        </div>
+    );
+};
+
+export default BreweryPage; 
