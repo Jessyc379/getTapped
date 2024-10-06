5994adc7796ed61c976a4623f3add53ac4acc085
Karen Delarosa <kaydelarose@gmail.com>
2024-10-06 14:54:56 -0700

    created Brewery model + context
    (React App)

---
+++ react-web-app/src/models/brewery/Brewery.ts
@@ -0,0 +1,17 @@
+export class Brewery {
+
+    breweryId!: string;
+    breweryName!: string;
+    breweryType!: string;
+    address!: string;
+    city!: string;
+    stateProvince!: string;
+    postalCode!: string;
+    country!: string;
+    longitude!: number;
+    latitude!: number;
+    phone!: string;
+    websiteUrl!: string;
+    brewerId!: number;
+
+}
