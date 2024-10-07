cce6a448eeab6212a479359c213ae8d24b73807a
Karen Delarosa <kaydelarose@gmail.com>
2024-10-07 09:26:27 -0700

    created brewery service

---
+++ react-web-app/src/services/brewery-service/BreweryService.tsx
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { Brewery } from '../../models/brewery/Brewery';
+
+class BreweryService {
+
+    baseUrl = `${import.meta.env.VITE_API_BASE_URL}/breweries`
+
+    async getAllBreweries(): Promise<Brewery[]> {
+
+        const response = await axios.get<Brewery[]>(this.baseUrl)
+        return response.data
+
+    }
+
+    async addBrewery(brewery: Brewery): Promise<Brewery> {
+
+        const response = await axios.post<Brewery>(this.baseUrl, brewery)
+        return response.data
+
+    }
+
+    async updateBrewery(brewery: Brewery): Promise<void> {
+
+        const url = `${this.baseUrl}/${brewery.breweryId}`
+        await axios.put<void>(url, brewery)
+    }
+
+    async deleteBrewery(id: number) {
+
+        const url = `${this.baseUrl}/${this.baseUrl}/${id}`
+        await axios.delete<void>(url)
+    }
+
+
+}
+
+const breweryService = new BreweryService()
+export default breweryService
