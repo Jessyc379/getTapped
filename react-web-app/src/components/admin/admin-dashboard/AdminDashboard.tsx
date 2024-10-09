07bb8445ce534518002e4e31166ed2fb8ac69fc4
Charletta Harris <icharharris@gmail.com>
2024-10-08 23:15:23 -0700

    Auto stash before merge of "main" and "updatingadmin"

--- react-web-app/src/components/admin/admin-dashboard/AdminDashboard.tsx
+++ react-web-app/src/components/admin/admin-dashboard/AdminDashboard.tsx
@@ -1,28 +1,21 @@
-import { Link, useParams } from "react-router-dom"
+import React from "react";
+import { Link, useParams } from "react-router-dom";
 
 
-export default function AdminDashboard()
-{
-    const params = useParams()
-
+const AdminPage = () => {
+    const params = useParams();
 
     return (
-       
-    <>
- 
     <div className="container">
         <Link to={"/admin/profile"} className="card form-control text-center p-4">View My Admin Profile </Link>
 
-        <div className="card form-control text-center p-4">
+        <div className="card form-control text-center p-4" mt-3>
             <h4> Manage Breweries: </h4>
 
             <Link to={"/brewery/view"} className="btn btn-outline-success mt-2">View All</Link>
             <Link to={"/brewery/manage"} className="btn btn-outline-success mt-2">Manager Breweries</Link>
-            
-
-
-
-
+        </div>   
+    
             <div className="card form-control mt-3">
                 <h4>Manage Brewers: </h4>
                 
@@ -30,8 +23,15 @@ export default function AdminDashboard()
                 <Link to={"/admin/manage-brewer"} className="btn btn-outline-success mt-2">Edit </Link>
                 
         </div>
+    </div>
+    );
 
+<<<<<<< Updated upstream
         </div>
         
 
 </>
+=======
+};
+export default AdminPage;
+>>>>>>> Stashed changes
